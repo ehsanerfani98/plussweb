ead1c25e5eb1a6b1c2880138a7528a99084f1b65
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-09 03:00:42 +0330

    add post

--- inc/script.php
+++ inc/script.php
@@ -38,6 +38,7 @@ function header_scripts()
                 "datePublished": "<?= get_the_date() ?>",
                 "category": "<?php foreach((get_the_category()) as $category) { $postcat= $category->cat_ID; $catname =$category->cat_name; echo $catname.','; } ?>",
                 "image": "<?= get_the_post_thumbnail_url() ?>"
+                "video": "https://www.youtube.com/embed/<?= $maktabyar_post_options['opt-youtube-video-code'] ?>"
             }
         </script>
     <?php endif; ?>
