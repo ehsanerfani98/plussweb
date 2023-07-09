7a17e97a25ce13afb43fd046c00875ced854bb2f
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-09 15:41:56 +0330

    test

--- template-parts/home.php
+++ template-parts/home.php
@@ -26,33 +26,33 @@
 </div>
 
 <div class="container-fluid my-5">
-        <div class="owl-carousel card-product-slider owl-theme">
-            <?php
-            $args = [
-                'post_type' => 'post',
-                'posts_per_page' => 6,
-                'post_status' => 'publish'
-            ];
-            $posts = new WP_Query($args);
-            if ($posts->have_posts()) :
-                while ($posts->have_posts()) :
-                    $posts->the_post();
-            ?>
-                    <a href="<?php the_permalink() ?>" class="card">
-                        <div class="content-card p-2">
-                            <div class="card-image-list">
-                                <?php the_post_thumbnail() ?>
-                            </div>
-                            <div class="card-title-list">
-                                <h2><?php the_title() ?></h2>
-                            </div>
+    <div class="owl-carousel card-product-slider owl-theme">
+        <?php
+        $args = [
+            'post_type' => 'post',
+            'posts_per_page' => 6,
+            'post_status' => 'publish'
+        ];
+        $posts = new WP_Query($args);
+        if ($posts->have_posts()) :
+            while ($posts->have_posts()) :
+                $posts->the_post();
+        ?>
+                <a href="<?php the_permalink() ?>" class="card">
+                    <div class="content-card p-2">
+                        <div class="card-image-list">
+                            <?php the_post_thumbnail() ?>
                         </div>
-                    </a>
-            <?php
-                endwhile;
-                wp_reset_postdata();
-                wp_reset_query();
-            endif;
-            ?>
-        </div>
+                        <div class="card-title-list">
+                            <h2><?php the_title() ?></h2>
+                        </div>
+                    </div>
+                </a>
+        <?php
+            endwhile;
+            wp_reset_postdata();
+            wp_reset_query();
+        endif;
+        ?>
+    </div>
 </div>
