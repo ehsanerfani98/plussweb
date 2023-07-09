1f94d3a511429ed5216d231bec59ae8ff717a471
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-09 15:24:22 +0330

    test

--- template-parts/home.php
+++ template-parts/home.php
@@ -25,7 +25,7 @@
     </div>
 </div>
 
-<div class="container">
+<div class="container-fluid my-5">
         <div class="owl-carousel card-product-slider owl-theme">
             <?php
             $args = [
