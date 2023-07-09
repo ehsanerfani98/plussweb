6055166c5202a565e82f48d8dc2c4f3b4f26a5be
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-09 14:17:59 +0330

    test

--- template-parts/home.php
+++ template-parts/home.php
@@ -1,3 +1,28 @@
+<div class="row">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="https://plussweb.ir/wp-content/uploads/2023/07/ley4.png" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="https://plussweb.ir/wp-content/uploads/2023/07/youtube-alert-1.png" alt="Second slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
 <div class="row my-5">
     <?php
     $args = [
