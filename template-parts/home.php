11d1cb41e6fc52182fd457ec433f0f0d4b050a43
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-09 16:13:55 +0330

    test

--- template-parts/home.php
+++ template-parts/home.php
@@ -18,6 +18,9 @@
 </div>
 
 <div class="container-fluid my-5">
+    <div class="row">
+        <h4>دانش وردپرسیت رو منفجر کن!</h4>
+    </div>
     <div class="owl-carousel card-product-slider owl-theme">
         <?php
         $args = [
