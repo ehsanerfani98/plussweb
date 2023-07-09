34e72f5a1cdca856295eea91798df3427b6767d8
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-09 14:42:18 +0330

    test

--- template-parts/home.php
+++ template-parts/home.php
@@ -1,19 +1,27 @@
 <div class="row">
     <div class="col-12 px-0">
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="https://plussweb.ir/wp-content/uploads/2023/07/ley4.png" alt="First slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="https://plussweb.ir/wp-content/uploads/2023/07/youtube-alert-1.png" alt="Second slide">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="https://plussweb.ir/wp-content/uploads/2023/07/ley4.png" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://plussweb.ir/wp-content/uploads/2023/07/youtube-alert-1.png" alt="Second slide">
+                </div>
             </div>
         </div>
-    </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">بلی</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">بعدی</span>
+        </a>
     </div>
 </div>
 
@@ -52,5 +60,4 @@
 
 <script>
     $('.carousel').carousel();
-
 </script>
