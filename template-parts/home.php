3a22f520f3f6c8694fbf5f8001722c6b32fb79d2
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-09 14:19:08 +0330

    test

--- template-parts/home.php
+++ template-parts/home.php
@@ -1,4 +1,5 @@
 <div class="row">
+    <div class="col-12">
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -20,6 +21,7 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
+    </div>
     </div>
 </div>
 
