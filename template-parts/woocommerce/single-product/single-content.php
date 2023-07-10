bd19102cec1c98a9a75b17b478dfedb35dd83db8
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-10 22:43:52 +0330

    test

--- template-parts/woocommerce/single-product/single-content.php
+++ template-parts/woocommerce/single-product/single-content.php
@@ -1,19 +1,22 @@
-<?php // do_action('view_content_card'); ?>
+<?php // do_action('view_content_card'); 
+?>
 <?php $PLSWB_COURSE_OPTION = get_post_meta(get_the_ID(), 'PLSWB_COURSE_OPTION', true); ?>
 
-<?php if ($PLSWB_COURSE_OPTION['opt-show-youtube-video']) :  ?>
-    <?php if (!empty($PLSWB_COURSE_OPTION['opt-youtube-video-code'])) :  ?>
-        <div class="video-card mt-4 mt-lg-0">
-            <iframe title="<?php the_title() ?>" src="https://www.youtube.com/embed/<?= $PLSWB_COURSE_OPTION['opt-youtube-video-code']  ?>" width="100%" height="450" frameborder="0" allowfullscreen="allowfullscreen" data-mce-fragment="1"><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start">﻿</span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span></iframe>
-            <!-- <video width="100%" poster="https://plussweb.ir/wp-content/uploads/2023/03/thumbnail-youtube.png" controls>
+<?php if (isset($PLSWB_COURSE_OPTION['opt-show-youtube-video'])) :  ?>
+    <?php if ($PLSWB_COURSE_OPTION['opt-show-youtube-video']) :  ?>
+        <?php if (!empty($PLSWB_COURSE_OPTION['opt-youtube-video-code'])) :  ?>
+            <div class="video-card mt-4 mt-lg-0">
+                <iframe title="<?php the_title() ?>" src="https://www.youtube.com/embed/<?= $PLSWB_COURSE_OPTION['opt-youtube-video-code']  ?>" width="100%" height="450" frameborder="0" allowfullscreen="allowfullscreen" data-mce-fragment="1"><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start">﻿</span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span></iframe>
+                <!-- <video width="100%" poster="https://plussweb.ir/wp-content/uploads/2023/03/thumbnail-youtube.png" controls>
                 <source type="video/mp4" src="https://plussweb.ir/wp-content/uploads/2023/03/Rec-0004.mp4">
             </video> -->
+            </div>
+        <?php endif; ?>
+    <?php else : ?>
+        <div class="video-card mt-4 mt-lg-0">
+            <?php the_post_thumbnail() ?>
         </div>
     <?php endif; ?>
-<?php else : ?>
-    <div class="video-card mt-4 mt-lg-0">
-        <?php the_post_thumbnail() ?>
-    </div>
 <?php endif; ?>
 
 <div class="card">
