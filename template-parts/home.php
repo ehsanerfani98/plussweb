62ed256a5610e5a753021144391a0a9b071cc5ee
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-11 03:24:11 +0330

    test

--- template-parts/home.php
+++ template-parts/home.php
@@ -99,37 +99,3 @@
     </div>
 </div>
 
-
-<div class="row footer">
-    <div class="col-lg-9">
-        <div class="footer-content">
-            <div class="footer-menu">
-                <ul>
-                    <li><a href="#">سفارش طراحی سایت</a></li>
-                    <li><a href="#">استخدام</a></li>
-                    <li><a href="#">ارتباط با ما</a></li>
-                    <li><a href="#">درباره ما</a></li>
-                </ul>
-            </div>
-            <div class="copy-right">
-                <h6>کپی از محتوای این سایت تحت هیچ شرایطی مجاز نیست.</h6>
-            </div>
-            <div class="social">
-                <a href="#">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <a href="#">
-                    <i class="fab fa-youtube"></i>
-                </a>
-                <a href="#">
-                    <i class="fab fa-linkedin"></i>
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-3">
-        <div class="footer-permission">
-
-        </div>
-    </div>
-</div>
