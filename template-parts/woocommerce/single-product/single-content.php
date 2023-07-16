45f23b7b79f9fc933590edcfde8d02d579f83c41
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-16 15:20:38 +0330

    http tp https api ip

--- template-parts/woocommerce/single-product/single-content.php
+++ template-parts/woocommerce/single-product/single-content.php
@@ -13,7 +13,7 @@
                     'verify_peer_name'   => FALSE,
                     'allow_self_signed'  => TRUE
                      )));
-              $country = unserialize(file_get_contents('https://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR']), false)['geoplugin_countryName'];
+              $country = unserialize(file_get_contents('https://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR'], false))['geoplugin_countryName'];
             if ($country == 'Iran') : ?>
                        <div style="padding: 10px; background: #ff5757; margin: 0 0 20px 0; color: white; border-radius: 4px; text-align: center; box-shadow: 0px 0px 17px 0px #fb8988;">برای تماشای ویدیو ابتدا vpn خود را <span style="padding: 4px 10px; background: orange; border-radius: 6px;">روشن</span> کرده و صفحه را رفرش کنید. (ویدیو در یوتیوب بارگذاری شده است)</div>
             <?php else : ?>
