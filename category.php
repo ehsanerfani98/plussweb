78dedcea344a48e048f9bb9581f3da8fbecdae8a
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-18 00:11:15 +0330

    add archive page

---
+++ category.php
@@ -0,0 +1,39 @@
+<div class="container-fluid my-5">
+    <div class="row">
+        <div class="col-12">
+            <div class="list-title">
+                <div class="list-icon">
+                    <i class="fa fa-graduation-cap"></i>
+                </div>
+                <h4>دانش وردپرسی خودت رو منفجر کن!</h4>
+            </div>
+        </div>
+    </div>
+<?php
+        $args = [
+            'post_type' => 'post',
+            'posts_per_page' => 6,
+            'post_status' => 'publish'
+        ];
+        $posts = new WP_Query($args);
+        if ($posts->have_posts()) :
+            while ($posts->have_posts()) :
+                $posts->the_post();
+        ?>
+                <a href="<?php the_permalink() ?>" class="card mt-4 mb-5 mx-2">
+                    <div class="content-card p-2">
+                        <div class="card-image-list">
+                            <?php the_post_thumbnail('normal', ['loading' => 'lazy']) ?>
+                        </div>
+                        <div class="card-title-list">
+                            <h2><?= mb_strlen(get_the_title())  > 65 ? mb_substr(get_the_title(), 0, 65) . '...' : get_the_title() ?></h2>
+                        </div>
+                    </div>
+                </a>
+        <?php
+            endwhile;
+            wp_reset_postdata();
+            wp_reset_query();
+        endif;
+        ?>
+</div>
