6f8a65f3eb69b77ec84fff60ae07120520251750
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-18 01:23:58 +0330

    add archive page

--- template-parts/header.php
+++ template-parts/header.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php if (is_singular()) : ?>
-        <meta property="og:title" content="How to Become an SEO Expert (8 Steps)" />
-        <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps." />
-        <meta property="og:image" content="https://ahrefs.com/blog/wp-content/uploads/2019/12/fb-how-to-become-an-seo-expert.png" />
+        <meta property="og:title" content="<?php the_title() ?>" />
+        <meta property="og:description" content="<?php the_excerpt() ?>" />
+        <meta property="og:image" content="<?= get_the_post_thumbnail_url() ?>" />
     <?php endif; ?>
 </head>
 
