8ccf9e87b38712658e730f6826e586f391ed5996
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-18 00:12:18 +0330

    add archive page

--- category.php
+++ category.php
@@ -1,3 +1,4 @@
+<?php get_header() ?>
 <div class="container-fluid my-5">
     <div class="row">
         <div class="col-12">
@@ -9,31 +10,33 @@
             </div>
         </div>
     </div>
-<?php
-        $args = [
-            'post_type' => 'post',
-            'posts_per_page' => 6,
-            'post_status' => 'publish'
-        ];
-        $posts = new WP_Query($args);
-        if ($posts->have_posts()) :
-            while ($posts->have_posts()) :
-                $posts->the_post();
-        ?>
-                <a href="<?php the_permalink() ?>" class="card mt-4 mb-5 mx-2">
-                    <div class="content-card p-2">
-                        <div class="card-image-list">
-                            <?php the_post_thumbnail('normal', ['loading' => 'lazy']) ?>
-                        </div>
-                        <div class="card-title-list">
-                            <h2><?= mb_strlen(get_the_title())  > 65 ? mb_substr(get_the_title(), 0, 65) . '...' : get_the_title() ?></h2>
-                        </div>
+    <?php
+    $args = [
+        'post_type' => 'post',
+        'posts_per_page' => 6,
+        'post_status' => 'publish'
+    ];
+    $posts = new WP_Query($args);
+    if ($posts->have_posts()) :
+        while ($posts->have_posts()) :
+            $posts->the_post();
+    ?>
+            <a href="<?php the_permalink() ?>" class="card mt-4 mb-5 mx-2">
+                <div class="content-card p-2">
+                    <div class="card-image-list">
+                        <?php the_post_thumbnail('normal', ['loading' => 'lazy']) ?>
                     </div>
-                </a>
-        <?php
-            endwhile;
-            wp_reset_postdata();
-            wp_reset_query();
-        endif;
-        ?>
+                    <div class="card-title-list">
+                        <h2><?= mb_strlen(get_the_title())  > 65 ? mb_substr(get_the_title(), 0, 65) . '...' : get_the_title() ?></h2>
+                    </div>
+                </div>
+            </a>
+    <?php
+        endwhile;
+        wp_reset_postdata();
+        wp_reset_query();
+    endif;
+    ?>
 </div>
+
+<?php get_footer() ?>
