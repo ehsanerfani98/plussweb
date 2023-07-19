9e54aacb72553413147e4c96d32daf18d7075242
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-20 00:34:12 +0330

    info box option

--- template-parts/woocommerce/single-product/single-sidebar.php
+++ template-parts/woocommerce/single-product/single-sidebar.php
@@ -1,7 +1,7 @@
 <?php
 $product = wc_get_product(get_the_ID());
 $rating  = $product->get_average_rating();
-var_dump($PLSWB_COURSE_OPTION['opt-info-box-volume']);
+var_dump($PLSWB_COURSE_OPTION);
 ?>
 
 <div class="card-v2">
