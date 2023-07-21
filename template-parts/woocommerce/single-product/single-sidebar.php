dc22a4e50c18c3c45318e932381ccb6ce2057eb3
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-21 23:10:24 +0330

    add price single product

--- template-parts/woocommerce/single-product/single-sidebar.php
+++ template-parts/woocommerce/single-product/single-sidebar.php
@@ -44,7 +44,7 @@ $rating  = $product->get_average_rating();
                             <span class="title">قیمت دوره</span>
                             <span class="price">
 
-                                 <?php if ($product->get_sale_price() && $product->get_sale_price() != 0 && $product->get_sale_price() != '') : ?>
+                                <?php if ($product->get_sale_price() && $product->get_sale_price() != 0 && $product->get_sale_price() != '') : ?>
                                     <del><?= $product->get_regular_price() . ' ' . get_woocommerce_currency_symbol() ?></del>
                                     <span class="orginal"><?= $product->get_sale_price() . ' ' . get_woocommerce_currency_symbol() ?></span>
                                 <?php else : ?>
@@ -66,8 +66,14 @@ $rating  = $product->get_average_rating();
                     <div class="product-order">
                         <span class="title">قیمت دوره</span>
                         <span class="price">
-                            <del>۳۵۰,۰۰۰ تومان</del>
-                            <span class="orginal">رایـــــگـان</span>
+                            <?php if ($product->get_sale_price() && $product->get_sale_price() != 0 && $product->get_sale_price() != '') : ?>
+                                <del><?= $product->get_regular_price() . ' ' . get_woocommerce_currency_symbol() ?></del>
+                                <span class="orginal"><?= $product->get_sale_price() . ' ' . get_woocommerce_currency_symbol() ?></span>
+                            <?php else : ?>
+                                <span class="orginal"><?= $product->get_regular_price() . ' ' . get_woocommerce_currency_symbol() ?></span>
+                            <?php endif; ?>
+                            <!-- <del>۳۵۰,۰۰۰ تومان</del> -->
+                            <!-- <span class="orginal">رایـــــگـان</span> -->
                             <!-- <span class="orginal">۱۰۰۰,۰۰۰ تومان</span> -->
                         </span>
                     </div>
