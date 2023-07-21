0602500621e081d9ad54a151ae989f9d841c5941
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-22 00:50:56 +0330

    add price single product

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -178,6 +178,8 @@ do_action('woocommerce_before_cart'); ?>
 				?>
 			</div>
 			<button type="submit" class="button<?php echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : ''); ?>" name="update_cart" value="<?php esc_attr_e('Update cart', 'woocommerce'); ?>"><?php esc_html_e('Update cart', 'woocommerce'); ?></button>
+			<?php do_action('woocommerce_cart_actions'); ?>
+			<?php wp_nonce_field('woocommerce-cart', 'woocommerce-cart-nonce'); ?>
 		</div>
 
 	</div>
