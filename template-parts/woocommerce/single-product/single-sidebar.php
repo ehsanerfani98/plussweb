a48a8c080aeb533b317353cbca4fde0e22b1b8eb
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-21 23:06:38 +0330

    add price single product

--- template-parts/woocommerce/single-product/single-sidebar.php
+++ template-parts/woocommerce/single-product/single-sidebar.php
@@ -43,8 +43,16 @@ $rating  = $product->get_average_rating();
                         <div class="product-order">
                             <span class="title">قیمت دوره</span>
                             <span class="price">
-                                <del>۳۵۰,۰۰۰ تومان</del>
-                                <span class="orginal">رایـــــگـان</span>
+
+                                 <?php if ($product->get_sale_price() && $product->get_sale_price() != 0 && $product->get_sale_price() != '') : ?>
+                                    <del><?= latin2farsi($product->get_regular_price()) . ' ' . get_woocommerce_currency_symbol() ?></del>
+                                    <span class="orginal"><?= latin2farsi($product->get_sale_price()) . ' ' . get_woocommerce_currency_symbol() ?></span>
+                                <?php else : ?>
+                                    <span class="orginal"><?= latin2farsi($product->get_regular_price()) . ' ' . get_woocommerce_currency_symbol() ?></span>
+                                <?php endif; ?>
+
+                                <!-- <del>۳۵۰,۰۰۰ تومان</del>
+                                <span class="orginal">رایـــــگـان</span> -->
                                 <!-- <span class="orginal">۱۰۰۰,۰۰۰ تومان</span> -->
                             </span>
                         </div>
