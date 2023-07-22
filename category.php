a16612eae64baee59f9bebaa785fa1a3d6ba9187
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-22 22:17:11 +0330

    change notices style

--- category.php
+++ category.php
@@ -1,12 +1,4 @@
 <?php get_header() ?>
-<div class="row">
-    <div class="header-product-single" style="background-image: url('<?= PLSWB_THEME_ASSETS . 'images/icon-back-OWL.png' ?>');">
-        <div class="title">
-            <h1><?php single_cat_title() ?></h1>
-        </div>
-        <?php woocommerce_breadcrumb() ?>
-    </div>
-</div>
 <div class="container-fluid my-5">
     <div class="row">
         <div class="col-12">
