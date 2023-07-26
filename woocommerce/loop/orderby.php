bb155026a4a4ee01e441e8b752f37d6462441eca
ehsanerfani98 <you@example.com>
2023-07-26 10:58:34 -0700

    archive product

--- woocommerce/loop/orderby.php
+++ woocommerce/loop/orderby.php
@@ -20,6 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
+<div class="col-12 col-lg-6">
 <form class="woocommerce-ordering" method="get">
 	<select name="orderby" class="orderby" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
 		<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
@@ -29,4 +30,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<input type="hidden" name="paged" value="1" />
 	<?php wc_query_string_form_fields( null, array( 'orderby', 'submit', 'paged', 'product-page' ) ); ?>
 </form>
+</div>
 </div>
