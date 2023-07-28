0b08ce3dec223a76902d926846f27c66f779062d
ehsanerfani98 <you@example.com>
2023-07-28 12:11:10 -0700

    checkout

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -39,7 +39,7 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
 
 	<div class="col2-set" id="customer_details">
 		<div class="col-1">
-			<div class="card">
+			<div class="card p-4">
 				<?php do_action('woocommerce_checkout_before_order_review_heading'); ?>
 
 				<h3 id="order_review_heading"><?php esc_html_e('Your order', 'woocommerce'); ?></h3>
@@ -52,7 +52,6 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
 
 				<?php do_action('woocommerce_checkout_after_order_review'); ?>
 			</div>
-
 		</div>
 
 		<div class="col-2">
@@ -60,7 +59,7 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
 			//  do_action( 'woocommerce_checkout_shipping' );
 
 			?>
-			<div class="card">
+			<div class="card p-4">
 				<?php do_action('woocommerce_checkout_billing'); ?>
 			</div>
 		</div>
