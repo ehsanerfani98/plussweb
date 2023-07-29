675bcca3218e5feeb5f3176da7af134a4518e3b8
ehsanerfani98 <you@example.com>
2023-07-28 23:13:59 -0700

    headlines

--- template-parts/woocommerce/single-product/single-headlines.php
+++ template-parts/woocommerce/single-product/single-headlines.php
@@ -36,5 +36,42 @@
                     </div>
                 </div>
             </div>
+            <div class="card">
+                <div class="card-header" id="headingOne">
+                    <h2 class="mb-0 d-flex align-items-center">
+                        <div class="list-icon">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <button class="btn btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            فصل اول
+                        </button>
+                    </h2>
+                </div>
+
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <div class="item-headline">
+                            <div class="item-headline-title">
+                                قسمت اول دوره آموزشی سرور کشینگ
+                            </div>
+                            <a href="#" class="item-headline-link-download">
+                                <div class="list-icon">
+                                    <i class="fa fa-download"></i>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="item-headline">
+                            <div class="item-headline-title">
+                                قسمت دوم دوره آموزشی سرور کشینگ
+                            </div>
+                            <div class="item-headline-link-download">
+                                <div class="list-icon red-icon">
+                                    <i class="fa fa-lock"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
