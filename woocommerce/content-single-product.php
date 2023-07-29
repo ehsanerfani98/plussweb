2eb15aa535baed0b50d5b29d396bc68c6cebe762
ehsanerfani98 <you@example.com>
2023-07-28 22:31:50 -0700

    headlines

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -41,7 +41,7 @@ if (post_password_required()) {
 	<div class="col-12 col-sm-12 col-md-12 col-lg-8">
 		<div class="header-tab mb-3">
 			<a href="<?= get_permalink() ?>" class="tab-item <?= get_query_var('pagename') == '' ? 'tab-active' : '' ?>">توضیحات</a>
-			<a href="#" class="tab-item">سرفصل ها</a>
+			<a href="<?= get_permalink() . 'headlines' ?>" class="tab-item <?= get_query_var('pagename') == 'headlines' ? 'tab-active' : '' ?>">سرفصل ها</a>
 			<a href="<?= get_permalink() . 'discussions' ?>" class="tab-item <?= get_query_var('pagename') == 'discussions' ? 'tab-active' : '' ?>">پرسش و پاسخ</a>
 			<a href="#" class="tab-item">سوالات متداول</a>
 		</div>
