57e0f6069806ddc471dc27c30e0fffd3dd5319d5
ehsanerfani98 <you@example.com>
2023-08-02 09:09:24 -0700

    remove ip

--- single-post.php
+++ single-post.php
@@ -9,19 +9,13 @@
             <?php if ($maktabyar_post_options['opt-show-youtube-video']) :  ?>
                 <?php if (!empty($maktabyar_post_options['opt-youtube-video-code'])) :  ?>
 
-                    <?php
-                    $country = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=' . $_SERVER['REMOTE_ADDR']))['geoplugin_countryName'];
-                    if ($country == 'Iran') : ?>
-
-                        <div style="padding: 10px; background: #ff5757; margin: 0 0 20px 0; color: white; border-radius: 4px; text-align: center; box-shadow: 0px 0px 17px 0px #fb8988;">برای تماشای ویدیو ابتدا vpn خود را <span style="padding: 4px 10px; background: orange; border-radius: 6px;">روشن</span> کرده و صفحه را رفرش کنید. (ویدیو در یوتیوب بارگذاری شده است)</div>
-                    <?php else : ?>
+                   
                         <div class="video-card mt-4 mt-lg-0">
                             <iframe title="<?php the_title() ?>" src="https://www.youtube.com/embed/<?= $maktabyar_post_options['opt-youtube-video-code']  ?>" width="100%" height="450" frameborder="0" allowfullscreen="allowfullscreen" data-mce-fragment="1"><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start">﻿</span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span></iframe>
                             <!-- <video width="100%" poster="https://plussweb.ir/wp-content/uploads/2023/03/thumbnail-youtube.png" controls>
                 <source type="video/mp4" src="https://plussweb.ir/wp-content/uploads/2023/03/Rec-0004.mp4">
             </video> -->
                         </div>
-                    <?php endif; ?>
 
                 <?php endif; ?>
             <?php else : ?>
