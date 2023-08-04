1a5d33f042ea347aaa123cdc851fbd3a39603a63
ehsanerfani98 <you@example.com>
2023-08-03 22:36:54 -0700

    add backend headline

--- template-parts/woocommerce/single-product/single-headlines.php
+++ template-parts/woocommerce/single-product/single-headlines.php
@@ -1,19 +1,19 @@
 <?php $headlines = $PLSWB_COURSE_OPTION['opt-headlines'];?>
 <div class="accordion" id="accordionExample">
-    <?php foreach ($headlines as $headline) : ?>
+    <?php foreach ($headlines as $index => $headline) : ?>
         <div class="card mb-2">
             <div class="card-header" id="headingOne">
                 <h2 class="mb-0 d-flex align-items-center">
                     <div class="list-icon">
                         <i class="fa fa-user"></i>
                     </div>
-                    <button class="btn btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <button class="btn btn-block text-left" type="button" data-toggle="collapse" data-target="#item<?= $index ?>" aria-expanded="true" aria-controls="collapseOne">
                         <?= $headline['opt-headline-title'] ?>
                     </button>
                 </h2>
             </div>
 
-            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div id="item<?= $index ?>" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
                 <div class="card-body">
                     <?php foreach ($headline['opt-headline-item'] as $item) : ?>
                         <div class="item-headline">
