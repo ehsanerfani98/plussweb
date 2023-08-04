c7ac4d0ce5294d838a3e82f11c9cff3264fa1738
ehsanerfani98 <you@example.com>
2023-08-03 22:26:00 -0700

    add backend headline

--- template-parts/woocommerce/single-product/single-headlines.php
+++ template-parts/woocommerce/single-product/single-headlines.php
@@ -1,4 +1,6 @@
-    <div class="accordion" id="accordionExample">
+<?php $headlines = $PLSWB_COURSE_OPTION['opt-headlines'];  ?>
+<div class="accordion" id="accordionExample">
+    <?php foreach ($headlines as $headline) : ?>
         <div class="card mb-2">
             <div class="card-header" id="headingOne">
                 <h2 class="mb-0 d-flex align-items-center">
@@ -36,41 +38,5 @@
                 </div>
             </div>
         </div>
-        <div class="card mb-2">
-            <div class="card-header" id="headingOne">
-                <h2 class="mb-0 d-flex align-items-center">
-                    <div class="list-icon">
-                        <i class="fa fa-user"></i>
-                    </div>
-                    <button class="btn btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        فصل اول
-                    </button>
-                </h2>
-            </div>
-
-            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                <div class="card-body">
-                    <div class="item-headline">
-                        <div class="item-headline-title">
-                            قسمت اول دوره آموزشی سرور کشینگ
-                        </div>
-                        <a href="#" class="item-headline-link-download">
-                            <div class="list-icon">
-                                <i class="fa fa-download"></i>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="item-headline">
-                        <div class="item-headline-title">
-                            قسمت دوم دوره آموزشی سرور کشینگ
-                        </div>
-                        <div class="item-headline-link-download">
-                            <div class="list-icon red-icon">
-                                <i class="fa fa-lock"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php endforeach; ?>
+</div>
